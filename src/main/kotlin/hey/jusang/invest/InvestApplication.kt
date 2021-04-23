94a203d04c072d017fc5d91bb21267bbe98ef0aa
heyjusang <co9901@gmail.com>
2021-04-23 17:07:23 +0900

    - AspectJ - logging start time, end time of service layer

--- src/main/kotlin/hey/jusang/invest/InvestApplication.kt
+++ src/main/kotlin/hey/jusang/invest/InvestApplication.kt
@@ -3,11 +3,13 @@ package hey.jusang.invest
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.EnableAspectJAutoProxy
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
 import java.time.Clock
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 class InvestApplication {
     @Bean
     fun passwordEncoder(): PasswordEncoder {
